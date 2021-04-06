c4f188ad5e0c07572583ce78eb647ab1b8b087bb
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2021-04-06 22:17:33 +0200

    added global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
-import type {AppProps} from 'next/app'
-import { GlobalStyles } from "twin.macro";
+import type { AppProps } from "next/app";
+import tw, { GlobalStyles, css } from "twin.macro";
+import { Global } from "@emotion/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
+      <Global
+        styles={css`
+          html,
+          body {
+            height: 100%;
+          }
+
+          #__next {
+            min-height: 100%;
+          }
+
+          body {
+            ${tw`bg-warmGray-50 dark:bg-warmGray-900`}
+          }
+        `}
+      />
       <Component {...pageProps} />
     </>
   );
